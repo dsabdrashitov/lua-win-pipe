f2f08a22effe17ba846b9ad25bb71673c6fdc253
dsabdrashitov <dsabdrashitov@users.noreply.github.com>
2022-05-09 20:48:02 +0300

    Make Makefile copy lua files to build.

--- src/lua-win-pipe.lua
+++ src/lua-win-pipe.lua
@@ -1,15 +1,16 @@
+lua_win_pipe = {}
+
 -- Change path
 local prev_path = package.path
 local root_path = debug.getinfo(1).source:match("@(.*\\)") or ""
 package.path = root_path .. "?.lua"
 
 -- imports and libs
-lua_win_pipe = require("build.lua-win-pipe.lua-win-pipe")
+local winpipe = package.loadlib(root_path .. "winpipe.dll", "luaopen_winpipe")()
 
 -- Restore path
 package.path = prev_path
 
-print(lua_win_pipe.dll.somefunc())
-for name, val in pairs(lua_win_pipe.dll.constants()) do
-    print(name .. ": " .. val)
-end
+lua_win_pipe.dll = winpipe
+
+return lua_win_pipe
